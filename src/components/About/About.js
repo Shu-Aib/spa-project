e797700c2feec90e925705c40a4804a6438f6b99
Shu-Aib <Shubsbehardien0@gmail.com>
2024-10-10 10:35:19 +0200

    added heading at home page and extra information in about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,7 +15,11 @@ export default function About() {
         </div>
       </header>
       <div className="about-content">
-       like the cars you love!
+       Like the cars you love! <br></br>
+       At Car Lovers, We believe that a picture is worth a thousand revs!
+       Our platform is designed for car enthusiasts, collectors, and anyone who simply appreciates the beauty and engineering 
+       of automobiles. Whether you're a fan of classic cars, modern marvels, or the latest supercars, we provide a space where you can 
+       explore, enjoy, and express your passion.
       </div>
     </div>
   );
--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
     <nav className='home-navbar'>
+      <h1 className='navbar-heading'>Car Lovers</h1>
         <ul>
             <Link to="/" className='nav-headings'><li>HOME</li></Link>
             <Link to="/about" className='nav-headings'><li>ABOUT</li></Link>
